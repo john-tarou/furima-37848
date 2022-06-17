b60ef48c18e9381222d8345de55be606b4e1a1a6
john-tarou <keiko06.mo@gmail.com>
2022-06-17 23:13:46 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :orders
+
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_06_15_131714) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
-    t.string "fisrt_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
