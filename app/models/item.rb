28303c2ab31530356149ed1a27c4dbf99835612e
john-tarou <keiko06.mo@gmail.com>
2022-06-20 22:07:26 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_sales_status
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_shipping_fee
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_scheduled_delivery
+
 end
