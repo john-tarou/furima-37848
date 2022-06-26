2c474ebff517bf189ee42d9d558cfedd86cd8163
john-tarou <keiko06.mo@gmail.com>
2022-06-27 00:20:10 +0900

    再修正_ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
   
   validates :image, presence: true
   validates :item_name, presence: true, length: { maximum: 40 }
