041b3720457ec1ebfa91a0285df549a7233b35d3
john-tarou <keiko06.mo@gmail.com>
2022-07-17 20:53:30 +0900

    Formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class orderAddress
+  include ActiveModel::Model
+  attr_accwssor :item_id, :user_id, :post_code, :item_prefecture_id, :city, :addresses, :building, :phone_number
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :addresses
+    validates :phone_number, numericality: { only_integer: true}, length: { minimum: 10, maximum: 11 }
+  end
+  validates :item_prefecture_id, numericality: {other_than: 0, message: "can't be blank" }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, item_prefecture_id: item_category_id, city: city, addresseses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
